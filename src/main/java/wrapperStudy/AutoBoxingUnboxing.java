865be766f7f42870a6e9f0fa369fa72926f2a614
kimse9450@gmail.com <kimse9450@gmail.com>
2019-01-28 19:49:37 +0900

    feat : wrapper class

---
+++ src/main/java/wrapperStudy/AutoBoxingUnboxing.java
@@ -0,0 +1,11 @@
+package wrapperStudy;
+
+public class AutoBoxingUnboxing {
+    public static void main(String[] args) {
+        Integer iValue = 10; // auto boxing
+        Integer iValue2 = 10; // auto boxing
+
+        int num1 = iValue; // auto unboxing
+        int num2 = iValue2; // auto unboxing
+    }
+}
