bcf86b83c526bacfc52006fc06b8c0bef0a765a4
kimse9450@gmail.com <kimse9450@gmail.com>
2019-03-13 16:54:35 +0900

    Create hackerrank Java Stdin and Stdout 1

---
+++ src/main/java/hackerrank/stdin_and_stdout/Solution.java
@@ -0,0 +1,19 @@
+package hackerrank.stdin_and_stdout;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int b = scan.nextInt();
+        scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int c = scan.nextInt();
+        scan.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
